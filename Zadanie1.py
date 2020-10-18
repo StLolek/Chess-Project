16616c00c8f734160d04ab7f1f814246fb5d6f5e
St.Lolek <passtwora@gmail.com>
2020-10-18 17:04:01 +0200

    Rozwiązanie zadania 1

---
+++ Zadanie1.py
@@ -0,0 +1,29 @@
+import chess
+import chess.pgn
+import time
+
+start_time = time.time()
+
+pgn = open("lichess_db_standard_rated_2013-01.pgn")
+moves = 0
+legalmoves = 0
+games = 0
+
+board = chess.Board()
+while True:
+    game = chess.pgn.read_game(pgn)
+    if game is None:
+        break  # end of file
+
+    games += 1
+    for move in game.mainline_moves():
+        legalmoves += board.legal_moves.count()
+        board.push(move)
+        moves += 1
+    board.reset()
+avgpergame = moves / games
+print("Średnia liczba wykonanych ruchów na gre: ", avgpergame)
+legalavgpermove = legalmoves / moves
+print("Średnia liczba dostępnych ruchów na ruch: ", legalavgpermove)
+print("\nZłożoność gry: ", avgpergame, "^", legalavgpermove, " = ", avgpergame ** legalavgpermove)
+print("\n--- %s seconds ---" % (time.time() - start_time))
